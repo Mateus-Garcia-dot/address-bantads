3dbc9dc00d15b58be975e15b139680d3c9e49cf5
Mateus Medeiros Garcia <mateus.medeiros@ufpr.br>
2023-02-11 15:00:49 -0300

    added consumer

---
+++ src/main/java/com/bantads/address/consumer/AddressConsumer.java
@@ -0,0 +1,43 @@
+package com.bantads.address.consumer;
+
+import com.bantads.address.model.AddressModel;
+import com.bantads.address.repository.AddressRepository;
+import lombok.Data;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Data
+@Component
+public class AddressConsumer {
+
+    private final AddressRepository addressRepository;
+
+    @RabbitListener(queues = "address.create")
+    public void createAddress(@RequestBody AddressModel addressModel) {
+       this.addressRepository.save(addressModel);
+    }
+
+    @RabbitListener(queues = "address.update")
+    public void updateAddress(@PathVariable Long id, @RequestBody AddressModel addressModel) {
+        AddressModel address = this.addressRepository.findById(id).orElse(null);
+        if (address == null) {
+            return;
+        }
+        address.setType(addressModel.getType());
+        address.setStreet(addressModel.getStreet());
+        address.setNumber(addressModel.getNumber());
+        address.setCity(addressModel.getCity());
+        address.setComplement(addressModel.getComplement());
+        address.setCep(addressModel.getCep());
+        address.setState(addressModel.getState());
+    }
+
+    @RabbitListener(queues = "address.delete")
+    public void deleteAddress(@PathVariable Long id) {
+        this.addressRepository.deleteById(id);
+    }
+
+}
